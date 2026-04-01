e8a4a46707add88b1c0974cf2e9375e06d57788c
frangiso <francoap6@gmail.com>
2026-04-01 01:43:23 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from './pages/Login'
 import Register from './pages/Register'
 import AlumnaPanel from './pages/AlumnaPanel'
 import ProfePanel from './pages/ProfePanel'
+import Migracion from './pages/Migracion'
 
 function RutaProtegida({ children, rol }) {
   const { user, perfil, loading } = useAuth()
@@ -23,14 +24,13 @@ export default function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/registro" element={<Register />} />
           <Route path="/alumna" element={
-            <RutaProtegida rol="alumna">
-              <AlumnaPanel />
-            </RutaProtegida>
+            <RutaProtegida rol="alumna"><AlumnaPanel /></RutaProtegida>
           } />
           <Route path="/profe" element={
-            <RutaProtegida rol="profe">
-              <ProfePanel />
-            </RutaProtegida>
+            <RutaProtegida rol="profe"><ProfePanel /></RutaProtegida>
+          } />
+          <Route path="/migracion" element={
+            <RutaProtegida rol="profe"><Migracion /></RutaProtegida>
           } />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
